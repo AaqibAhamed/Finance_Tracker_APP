e495cc563e672b0283087768f0e08a1743bcca75
Aaqib Wiki <aaqibgahamed@gmail.com>
2025-04-29 07:53:27 +0530

    Create Income.cs

---
+++ FinanceTrackerAPI/Models/Income.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceTrackerAPI.Models;
+
+public class Income
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Description is required.")]
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "Amount is required.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be a positive number.")]
+    public decimal Amount { get; set; }
+
+    [Required(ErrorMessage = "Date is required.")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [DateNotInFuture(ErrorMessage = "Date cannot be in the future.")]
+    public DateTime Date { get; set; }
+}
+
+public class DateNotInFutureAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value is DateTime date)
+        {
+            return date <= DateTime.Now.Date;
+        }
+        return false;
+    }
+}
