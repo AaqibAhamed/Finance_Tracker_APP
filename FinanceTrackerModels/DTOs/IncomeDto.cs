bf09632115f5066ee423760cea2641e859f95c7b
Aaqib Wiki <aaqibgahamed@gmail.com>
2025-05-02 15:45:02 +0530

    Change Date property to nullable in IncomeDto and update Amount validation

--- FinanceTrackerModels/DTOs/IncomeDto.cs
+++ FinanceTrackerModels/DTOs/IncomeDto.cs
@@ -10,15 +10,15 @@ namespace FinanceTrackerModels.DTOs
         public string? Description { get; set; }
 
         [Required(ErrorMessage = "Amount is required.")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be a positive number.")]
+        [Range(typeof(decimal), "1", "79228162514264337593543950335", ErrorMessage = "Amount must be a positive number.")]
         public decimal Amount { get; set; }
 
         [Required(ErrorMessage = "Date is required.")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DateNotInFuture(ErrorMessage = "The date cannot be in the future.")]
-        public required DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
     }
 
-    public record IncomeResponseDto(int Id, string Description, decimal Amount, DateTime Date);
+    public record IncomeResponseDto(int Id, string Description, decimal Amount, DateTime? Date);
 }
