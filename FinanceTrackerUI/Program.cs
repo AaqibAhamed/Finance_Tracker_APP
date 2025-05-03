f3bf16f2c500ff8a9593a5ff3671cce5282bd72f
Aaqib Wiki <aaqibgahamed@gmail.com>
2025-05-03 17:17:47 +0530

    Update API base URL in Program.cs to use HTTPS

--- FinanceTrackerUI/Program.cs
+++ FinanceTrackerUI/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5243/") }); // Use your API base URL
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7131") }); // Use your API base URL
 
 builder.Services.AddMudServices(); // Register MudBlazor services
 
