d39dffea93753bc7313fff7ad05fc28fe77ffe63
Aaqib Wiki <aaqibgahamed@gmail.com>
2025-05-03 12:20:18 +0530

    Add DTOs for Expense and Income data transfer

---
+++ FinanceTrackerModels/DTOs/GetIncomeDto.cs
@@ -0,0 +1,13 @@
+namespace FinanceTrackerModels.DTOs;
+
+public class GetIncomeDto
+{
+    public int Id { get; init; }
+    
+    public string? Description { get; set; }
+    
+    public decimal Amount { get; set; }
+    
+    public  DateTime? Date { get; set; }
+    
+}
